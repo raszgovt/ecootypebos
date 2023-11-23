d37a6112c9e369236d07ae62a213b2de101f5ffc
raszgovt <145519667+raszgovt@users.noreply.github.com>
2023-11-23 14:57:24 +0700

    Add files via upload

---
+++ ZType/ZType.js
@@ -0,0 +1,173 @@
+
+const WORDS = [ "bismillah", "astaghfirullah", "alhamdulillah", "subhanallah", "muhammad", "isa", "nuh", "ibrahim", "luth", "ismail" ];
+
+var focus; // Astroid the player is currently typing out
+var field = [];
+
+var score = 0;
+
+var planetCrust; // color of crust
+var planetMantle; // color of mantle
+
+var ship; // color of ship
+
+function setup() {
+
+  createCanvas(500, 500);
+
+  planetCrust = randomColor();
+  planetMantle = groundColor();
+  ship = randomColor();
+
+  field.push(new Asteroid(random(width - 150) + 75, 0, random(WORDS), randomColor()));
+
+  focus = null;
+}
+
+function draw() {
+
+  background(51);
+
+  drawBase();
+  drawLazer();
+	drawScore();
+
+  handleField();
+}
+
+/**
+ * updates & draws Astroids
+ * manages field array
+ * increments score
+ * manages focus
+ * creates Asteroids
+ */
+function handleField() {
+
+	for (var i = field.length - 1; i >= 0; i--) {
+
+    field[i].update();
+
+    if (field[i].intact) {
+			// astroid is still on-screen
+
+			field[i].draw();
+		} else {
+			// Astroid has been destroyed
+
+      score += field[i].text.length;
+      field.splice(i, 1); // delete from array
+      focus = null;
+    }
+  }
+
+	/* attempt new Astroid */
+  if (frameCount % 60 === 0) { // every second
+
+		if (random() > map(score, 0, 1000, 0.8, 0.01)) { // more difficult as game progresses
+
+			field.push(new Asteroid(random(width - 150) + 75, 0, random(WORDS), randomColor()));
+		}
+	}
+}
+
+/**
+ * handles user input
+ */
+function keyPressed() {
+
+  if (focus) {
+		// if we have honed in on a specific Asteroid
+
+    focus.erode(keyCode);
+  } else {
+		// find the astroid to target
+
+    focus = findAsteroid(keyCode, field);
+
+    if (focus) {
+      focus.erode(keyCode);
+    }
+  }
+}
+
+/**
+ * draws planet as a rectangle
+ * draws "ground control" as a triangle
+ */
+function drawBase() {
+
+  /* planet */
+  fill(groundColor());
+  stroke(groundColor());
+  strokeWeight(5);
+  rect(0, height - 15, width, height);
+
+  /* ground control */
+  fill(groundColor());
+  stroke(groundColor());
+  beginShape();
+  vertex(width / 2 - 20, height);
+  vertex(width / 2, height - 50);
+  vertex(width / 2 + 20, height);
+  endShape(CLOSE);
+}
+
+/**
+ * draws "lazer" between ground control and Asteroid
+ */
+function drawLazer() {
+
+  if (!focus)
+    return;
+
+  stroke(255);
+  strokeWeight(focus.completedText.length); // width of line depends on progress
+
+	// point of ground control
+  line(width / 2, height - 50, focus.position.x, focus.position.y);
+
+  fill(255);
+  noStroke();
+  textAlign(LEFT);
+  textSize(30);
+  text(focus.completedText, 10, height - 40);
+}
+
+/**
+ * draws the score
+ */
+function drawScore() {
+
+  textAlign(RIGHT);
+  noStroke();
+  textSize(30);
+  fill(255);
+  text(score, 50, height / 2);
+}
+
+/**
+ * Generates a random color
+ */
+function randomColor() {
+
+  return color(random(255), random(255), random(255));
+}
+
+function groundColor(){
+  return color(21,24,10);
+}
+
+/**
+ * stops loop, draws game over message
+ */
+function endGame() {
+
+  noLoop();
+
+	fill(255);
+  noStroke();
+  textAlign(CENTER);
+  textSize(80);
+	text("Game Over!", width / 2, height / 2);
+}
